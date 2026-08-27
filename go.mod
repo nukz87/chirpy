86e68bd41e99b8cfac6d3f1fe51e3fb2d5fbe00c
nukz87 <dihkuz0807@gmail.com>
2026-08-27 21:53:14 +0700

    get start

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nukz87/chirpy
+
+go 1.26.6
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthzHandler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
+}
